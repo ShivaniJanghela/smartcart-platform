f323de547edc8f833d2c742cf90b277e8ad8da7c
Shivani Janghela <shivaanni123@gmail.com>
2025-05-14 13:08:42 +0530

    Initial commit

---
+++ scripts/checkout.js
@@ -0,0 +1,91 @@
+let catItemArray=[
+    {
+        image:"images/products/athletic-cotton-socks-6-pairs.jpg",
+        name:" Black and Gray Athletic Cotton Socks - 6 Pairs",
+        price:1090,
+        quantity:2,
+    },
+    {
+        image:"images/products/intermediate-composite-basketball.jpg",
+        name:"Intermediate Size Basketball",
+        price:2095,
+        quantity:1,
+    }
+]
+let cartItem =``;
+catItemArray.forEach((item)=>{
+
+cartItem+=`
+ <div class="cart-item-details-grid">
+              <img class="product-image"
+                src=${item.image}>
+
+              <div class="cart-item-details">
+                <div class="product-name">
+                  ${item.name}
+                </div>
+                <div class="product-price">
+                  $${item.price}
+                </div>
+                <div class="product-quantity">
+                  <span>
+                    Quantity: <span class="quantity-label">${item.quantity}</span>
+                  </span>
+                  <span class="update-quantity-link link-primary">
+                    Update
+                  </span>
+                  <span class="delete-quantity-link link-primary">
+                    Delete
+                  </span>
+                </div>
+              </div>
+
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+`
+console.log(cartItemHtml)
+})
+document.querySelectorAll()
