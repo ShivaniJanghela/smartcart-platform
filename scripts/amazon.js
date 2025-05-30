c439105a6617e31ac95a42b5f2600b8eb34bcd1f
Shivani Janghela <shivaanni123@gmail.com>
2025-05-30 17:11:29 +0530

    Refactor: Convert products into classes.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
-import {formatCurrency} from "./utils/money.js"
+import { formatCurrency } from "./utils/money.js";
 let productHTML = ``;
 products.forEach((product) => {
   productHTML += `
@@ -17,7 +17,7 @@ products.forEach((product) => {
   
             <div class="product-rating-container">
               <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars * 10}.png">
+                src=${product.getStarURL()}>
               </img>
               <div class="product-rating-count link-primary">
                 ${product.rating.count}
@@ -25,7 +25,7 @@ products.forEach((product) => {
             </div>
   
             <div class="product-price">
-             $${formatCurrency(product.priceCents )}
+             ${product.getPrice()}
             </div>
        
 
